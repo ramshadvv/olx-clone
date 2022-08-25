c1e15787125bcafdda6194bbd7dec7010ebf3ec2
ravi-sankarp <ravisankar@pop-os.localdomain>
2022-08-25 16:44:50 +0530

    fixed links issue

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ function App() {
       <PostProvider>
 
       <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/signup' element={<Signup />}></Route>
-        <Route path='/create' element={<Create />}></Route>
-        <Route path='/viewpost' element={<ViewPost />}></Route>
+          <Route path='https://ravi-sankarp.github.io/olx-clone/' element={<Home />}></Route>
+          <Route path='https://ravi-sankarp.github.io/olx-clone/login' element={<Login />}></Route>
+          <Route path='https://ravi-sankarp.github.io/olx-clone/signup' element={<Signup />}></Route>
+          <Route path='https://ravi-sankarp.github.io/olx-clone/create' element={<Create />}></Route>
+          <Route path='https://ravi-sankarp.github.io/olx-clone/viewpost' element={<ViewPost />}></Route>
 
       </Routes>
       </PostProvider>
